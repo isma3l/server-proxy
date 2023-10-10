af99accbb41a0e5876688610bd0c3425302f46cf
isma3l <karlo.ismael@gmail.com>
2023-10-11 01:18:20 +0200

    temporary solution -  post

--- api/index.js
+++ api/index.js
@@ -30,6 +30,35 @@ const handler = (req, res) => {
   }
 }
 
+const handlerPost = (req, res) => {
+  async function postUrl(originalUrl) {
+    const url = originalUrl.replace("/api-post?url=", "")
+
+    try {
+      const response = await axios.post(url, req.body);
+      const contentType = response.headers.get("content-type");
+      res.setHeader("content-type", contentType)
+      res.status = response.status;
+
+      if (contentType.includes("json")) {
+        res.json(response.data);
+      } else {
+        res.send(response.data);
+      }
+
+    } catch (error) {
+      res.status = 500;
+      res.json({ type: 'error', message: error.message });
+    }
+  }
+  
+  if (req.query.url) {
+    postUrl(req.originalUrl);
+  } else {
+    res.send("missing url parameter");
+  }
+}
+
 const port = process.env.PORT || 3005;
 const app = express();
 Middlewares(app);
@@ -38,6 +67,9 @@ app.use("/api", (req, res) => {
   handler(req, res);
 });
 
+app.post("/api-post", (req, res) => {
+  handlerPost(req, res);
+});
 
 app.use('*', (_req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,9 @@
+import express from 'express';
 import cors from 'cors';
 
 const Middlewares = (app) => {
     app.use(cors());
+    app.use(express.json());
     app.use(function (req, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
